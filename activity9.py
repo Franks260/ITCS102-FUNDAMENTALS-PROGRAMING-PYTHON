60649dd8374980f076f46197d9a51ad72a2b34ad
Frank Louis Barranta <barrantafranklouis@gmail.com>
2025-08-21 19:27:59 +0800

    activity9 on comlab

---
+++ activity9.py
@@ -0,0 +1,7 @@
+username = 'Franks'
+password = 'sobrangpogikotalaga'
+
+print(username == 'Franks')
+print((username == 'Franks') and (password == 'sobrangpogikotalaga'))
+print((username == 'Franks') or (password == 'sobrangpogikotalaga'))
+print (not((username == 'Franks') or (password == 'sobrangpogikotalaga')))
