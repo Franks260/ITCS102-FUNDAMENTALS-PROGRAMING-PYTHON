5b0629364a1eb092de67fdfea99b9b1e423d94d9
Frank Louis Barranta <barrantafranklouis@gmail.com>
2025-08-21 19:25:31 +0800

    activity7 on comlab

---
+++ activity7.py
@@ -0,0 +1,15 @@
+a = 3
+
+print("The value of a is ", a)
+
+a += 57
+print("The value of a is ", a)
+
+a -= 21
+print("The value of a is ", a)
+
+a *= 65
+print("The value of a is ", a)
+
+a /= 11
+print("The value of a is ", a)
