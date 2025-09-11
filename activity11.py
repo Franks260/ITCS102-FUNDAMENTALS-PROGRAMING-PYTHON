c9fa29769d13ddef63158d8cd179afb2d5959347
Franks260 <barrantafranklouis@gmail.com>
2025-09-11 20:50:32 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,16 @@
+temp = eval(input("Enter temperature --> "))
+
+if temp < 1: 
+    print("Temperature freezing cold")
+elif temp >= 1 and temp <= 20:
+    print("Temperature cold")
+elif temp >= 21 and temp <= 30:
+    print("Temperature lukewarm")
+elif temp >= 31 and temp <= 40:
+    print("Temperature warm")
+elif temp >= 41 and temp <= 50:
+    print("Temperature hot")
+elif temp >= 51:
+    print("Temperature above boiling hot")
+else:
+    print("Invalid temperature")
