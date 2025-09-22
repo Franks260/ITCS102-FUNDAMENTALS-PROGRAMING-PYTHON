d4bf459762c0002c8de930924cbb272817a35e96
Franks260 <barrantafranklouis@gmail.com>
2025-09-22 21:02:05 +0800

    Add files via upload

---
+++ code_challange11.py
@@ -0,0 +1,17 @@
+for j in range(10,0,-1):
+    for g in range(1,j,1):
+        print(" ", end=" ")
+    for k in range(10,j,-1):
+        print("*",end=" ")
+    for o in range(11,j,-1):
+        print("*",end=" ")
+    print()
+
+for h in range (1,11,1):
+    for p in range(1,h,1):
+        print(" ",end=" ")
+    for z in range(10,h,-1):
+        print("*",end=" ")
+    for u in range(11,h,-1):
+        print("*",end=" ")
+    print()
