0eb937fb0579c28eb9d1e453ec64d806f63afbbd
Franks260 <barrantafranklouis@gmail.com>
2025-09-22 20:32:17 +0800

    Add files via upload

---
+++ code_challange10.py
@@ -0,0 +1,9 @@
+
+for j in range(10,0,-1):
+    for g in range(1,j,1):
+        print(" ", end=" ")
+    for k in range(10,j,-1):
+        print("*",end=" ")
+    for o in range(11,j,-1):
+        print("*",end=" ")
+    print()
