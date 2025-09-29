89067dde5a4ff6069b179939b7e8583c7ebc2b8c
Franks260 <barrantafranklouis@gmail.com>
2025-09-29 18:39:18 +0800

    Add files via upload

---
+++ code_challange13.py
@@ -0,0 +1,46 @@
+for j in range(5,0,-1):
+    for ji in range(1,7,1):
+        print(" ",end=" ")
+    for g in range(1,j,1):
+        print(" ", end=" ")
+    for k in range(4,j,-1):
+        print("*",end=" ")
+    for o in range(5,j,-1):
+        print("*",end=" ")
+    print()
+for h in range (1,5,1):
+    for df in range(1,7,1):
+        print(" ",end=" ")
+    for p in range(1,h,1):
+        print(" ",end=" ")
+    for z in range(4,h,-1):
+        print("*",end=" ")
+    for u in range(5,h,-1):
+        print("*",end=" ")
+    print()
+
+
+for j in range(10,0,-1):
+    for g in range(1,j,1):
+        print(" ", end=" ")
+    for k in range(10,j,-1):
+        print("*",end=" ")
+    for o in range(11,j,-1):
+        print("*",end=" ")
+    print()
+for j in range(10,0,-1):
+    for g in range(1,j,1):
+        print(" ", end=" ")
+    for k in range(10,j,-1):
+        print("*",end=" ")
+    for o in range(11,j,-1):
+        print("*",end=" ")
+    print()
+for j in range(10,0,-1):
+    for g in range(1,j,1):
+        print(" ", end=" ")
+    for k in range(10,j,-1):
+        print("*",end=" ")
+    for o in range(11,j,-1):
+        print("*",end=" ")
+    print()
