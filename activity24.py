7e0ca1e97b817c427ae252fe31492a471dd32c9d
Franks260 <barrantafranklouis@gmail.com>
2025-11-07 11:08:04 +0800

    Add files via upload

---
+++ activity24.py
@@ -0,0 +1,15 @@
+def greet(name):
+    print(f"Hello, {name} How are you?")
+
+greet("franks")
+greet("pedro")
+
+def summation(x):
+    sum = 0
+    for i in range(x,0,-1):
+        sum += i
+    print(f"The sum of {x} is {sum}")
+
+summation(4)
+summation(10)
+
---
+++ activity24_1.py
@@ -0,0 +1,7 @@
+from activity24 import summation, greet
+
+greet("flint and stein")
+greet("manok")
+
+summation(9)
+summation(11)
